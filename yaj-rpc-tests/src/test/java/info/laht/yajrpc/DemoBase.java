2d42cc8b44c1fc6184189a7d52f7234fa2459054
Lars Ivar Hatledal <laht@hials.no>
2018-11-06 15:41:26 +0100

    update

--- yaj-rpc-tests/src/test/java/info/laht/yajrpc/DemoBase.java
+++ yaj-rpc-tests/src/test/java/info/laht/yajrpc/DemoBase.java
@@ -6,27 +6,21 @@ import kotlin.Unit;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
 public class DemoBase {
 
-    static void run(RpcServer server, RpcClient client) throws IOException, TimeoutException, InterruptedException {
+    static void run(RpcServer server, RpcClient client) throws IOException, TimeoutException, InterruptedException, ExecutionException {
 
         try {
 
             client.notify("SampleService.returnNothing", RpcParams.noParams());
 
             RpcParams params = RpcParams.listParams("Clint Eastwood");
-            RpcResponse response = client.write("SampleService.greet", params);
+            RpcResponse response = client.write("SampleService.greet", params).get();
             String result = response.getResult(String.class); //prints 'Hello Client Eastwood!'
             System.out.println("Response=" + result);
-
-            client.writeAsync("SampleService.greet", params, res -> {
-                System.out.println(res.getResult(String.class)); //prints 'Hello Client Eastwood!'
-                return Unit.INSTANCE;
-            });
-
-            Thread.sleep(100);
 
             System.out.println("Press any key to exit..");
             Scanner sc = new Scanner(System.in);
