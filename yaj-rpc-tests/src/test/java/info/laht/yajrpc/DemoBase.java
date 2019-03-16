124ed575d0130417388309b4af97ad316e397e19
Lars Ivar Hatledal <laht@hials.no>
2019-03-16 09:30:11 +0100

    no need to wait for user input anymore

--- yaj-rpc-tests/src/test/java/info/laht/yajrpc/DemoBase.java
+++ yaj-rpc-tests/src/test/java/info/laht/yajrpc/DemoBase.java
@@ -2,10 +2,8 @@ package info.laht.yajrpc;
 
 import info.laht.yajrpc.net.RpcClient;
 import info.laht.yajrpc.net.RpcServer;
-import kotlin.Unit;
 
 import java.io.IOException;
-import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
@@ -21,12 +19,6 @@ public class DemoBase {
             RpcResponse response = client.write("SampleService.greet", params).get();
             String result = response.getResult(String.class); //prints 'Hello Client Eastwood!'
             System.out.println("Response=" + result);
-
-            System.out.println("Press any key to exit..");
-            Scanner sc = new Scanner(System.in);
-            if (sc.hasNext()) {
-                System.out.println("exiting..");
-            }
 
         } finally {
 
